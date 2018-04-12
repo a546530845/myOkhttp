7aa15b1e3153dae87c5d4099699c7721eefbf3df
a546530845 <546530845@qq.com>
2018-04-12 11:40:32 +0800

    修改

--- app/src/main/java/com/zxg/myokhttp/config/ApiConstant.java
+++ app/src/main/java/com/zxg/myokhttp/config/ApiConstant.java
@@ -10,7 +10,7 @@ public class ApiConstant {
     /**
      * app host develop
      */
-//    private static final String APP_SERVER_NAME = "http://192.168.1.216:8088/appserver/app_api/";
+    private static final String APP_SERVER_NAME = "http://192.168.1.216:8088/appserver/app_api/";
 //    private static final String APP_SERVER_NAME = "http://192.168.1.118:8088/appserver/app_api/";
 //    private static final String APP_SERVER_NAME = "http://192.168.1.177:8080/appserver/app_api/";
 //    private static final String APP_SERVER_NAME = "http://192.168.1.66:8080/appserver/app_api/";
@@ -18,14 +18,7 @@ public class ApiConstant {
     //测试
 //    private static final String APP_SERVER_NAME = "http://192.168.1.115:8088/appserver/app_api/";
 //    private static final String APP_SERVER_NAME = "http://192.168.1.221:8088/appserver/app_api/";
-    //预发布
-      private static final String APP_SERVER_NAME = "https://uat.dashufs.com/appserver/app_api/";
-    //生产
-//    private static final String APP_SERVER_NAME = "https://prod.dashufs.com/appserver/app_api/";
 
-    //外网测试
-//    private static final String APP_SERVER_NAME = "http://120.77.65.78:59988/appserver/app_api/";//开发
-//    private static final String APP_SERVER_NAME = "http://120.77.65.78:59989/appserver/app_api/";//测试
     /*
      * download apk
      */
